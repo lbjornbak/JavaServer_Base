d142f1de31963ff9a4e6729478b0026c56bb136f
Lars Nordahl Bjørnbak <larsnb@getmail.no>
2023-10-26 13:21:34 +0200

    Expand existing Person class. Make PersonDao class with access to db. Setup basic db logic. Update pom for these purposes.

--- src/main/java/no/http/Person.java
+++ src/main/java/no/http/Person.java
@@ -1,22 +1,41 @@
 package no.http;
 
 public class Person {
+    private Long id;
     private String firstName;
     private String lastName;
 
-    public String getFirstName() {
-        return firstName;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getFirstName() {
+        return firstName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
     }
 }
---
+++ src/main/java/no/http/dao/PersonDao.java
@@ -0,0 +1,70 @@
+package no.http.dao;
+
+import no.http.Person;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonDao {
+
+    private final DataSource dataSource;
+
+    public PersonDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public static void main(String[] args) throws SQLException {
+
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/person_db");
+        dataSource.setUser("person_dbuser");
+        dataSource.setPassword("(c)X?5<4s3<Y]AC5`9");
+
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("select * from people")) {
+
+                try (ResultSet rs = statement.executeQuery()) {
+                    while (rs.next()) {
+                        System.out.println(rs.getString("last_name"));
+                    }
+                }
+            }
+        }
+    }
+
+    public void save(Person person) throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("insert into people (first_name, last_name) values (?, ?)", PreparedStatement.RETURN_GENERATED_KEYS)) {
+                statement.setString(1, person.getFirstName());
+                statement.setString(2, person.getLastName());
+                statement.executeUpdate();
+
+                ResultSet rs = statement.getGeneratedKeys();
+                rs.next();
+                person.setId(rs.getLong("id"));
+            }
+        }
+    }
+
+    public Person retrieve(long id) throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("select * from people where id = ?")) {
+                statement.setLong(1, id);
+                ResultSet rs = statement.executeQuery();
+
+                if (rs.next()) {
+                    Person person = new Person();
+                    person.setId(rs.getLong("id"));
+                    person.setFirstName(rs.getString("first_name"));
+                    person.setLastName(rs.getString("last_name"));
+                    return person;
+                }
+            }
+        }
+        return null;
+    }
+}
