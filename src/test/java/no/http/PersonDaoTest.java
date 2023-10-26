2d4498a45df82529f751f6fe82cf424d70a01be8
Lars Nordahl Bjørnbak <larsnb@getmail.no>
2023-10-26 13:29:00 +0200

    Add some base tests for the PersonDao

---
+++ src/test/java/no/http/PersonDaoTest.java
@@ -0,0 +1,47 @@
+package no.http;
+
+import no.http.dao.PersonDao;
+import org.junit.jupiter.api.Test;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PersonDaoTest {
+
+    @Test
+    void shouldRetrieveSavedPerson() throws SQLException {
+        PersonDao dao = new PersonDao(createDataSource());
+
+        Person person = randomPerson();
+        dao.save(person);
+        assertThat(dao.retrieve(person.getId()))
+                .hasNoNullFieldsOrProperties()
+                .usingRecursiveComparison()
+                .isEqualTo(person);
+    }
+
+    private DataSource createDataSource() {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/person_db");
+        dataSource.setUser("person_dbuser");
+        dataSource.setPassword("(c)X?5<4s3<Y]AC5`9");
+        return dataSource;
+    }
+
+    private Person randomPerson() {
+        Person person = new Person();
+        person.setFirstName(pickOne("Johannes", "Jane", "Jonas", "Josephine", "Jamal"));
+        person.setLastName(pickOne("Persson", "Olsson", "Jensen", "Knutsen"));
+        return person;
+    }
+
+    private String pickOne(String... alternatives) {
+        return alternatives[new Random().nextInt(alternatives.length)];
+    }
+
+
+}
