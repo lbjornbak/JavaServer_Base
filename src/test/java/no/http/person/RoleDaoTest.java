4b1d92c7cdd8f2a491b5a2d8feaf9b2c9bb3c057
Lars Nordahl Bjørnbak <larsnb@getmail.no>
2023-10-27 12:50:53 +0200

    Create RoleDao and tests

---
+++ src/test/java/no/http/person/RoleDaoTest.java
@@ -0,0 +1,26 @@
+package no.http.person;
+
+import no.http.dao.RoleDao;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RoleDaoTest {
+
+    private RoleDao dao = new RoleDao(TestData.testDataSource());
+
+    @Test
+    void shouldListSavedRoles() throws SQLException {
+        String role1 = "role-" + UUID.randomUUID();
+        String role2 = "role-" + UUID.randomUUID();
+
+        dao.save(role1);
+        dao.save(role2);
+
+        assertThat(dao.listAll())
+                .contains(role1, role2);
+    }
+}
