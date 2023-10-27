9452183be397f36703c8be534500199c5723b82e
Lars Nordahl Bjørnbak <larsnb@getmail.no>
2023-10-27 09:52:50 +0200

    Expand PersonDao, add flyway.

--- src/main/java/no/http/dao/PersonDao.java
+++ src/main/java/no/http/dao/PersonDao.java
@@ -1,13 +1,17 @@
 package no.http.dao;
 
 import no.http.Person;
+import org.flywaydb.core.Flyway;
 import org.postgresql.ds.PGSimpleDataSource;
-
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class PersonDao {
 
@@ -17,54 +21,92 @@ public class PersonDao {
         this.dataSource = dataSource;
     }
 
-    public static void main(String[] args) throws SQLException {
-
+    public static DataSource createDataSource() {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
         dataSource.setUrl("jdbc:postgresql://localhost:5432/person_db");
         dataSource.setUser("person_dbuser");
-        dataSource.setPassword("(c)X?5<4s3<Y]AC5`9");
+        dataSource.setPassword("postgrespass");
 
-        try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement("select * from people")) {
+        //Username and password. Will be moved to separate file. Is here now for demo purposes
 
-                try (ResultSet rs = statement.executeQuery()) {
-                    while (rs.next()) {
-                        System.out.println(rs.getString("last_name"));
-                    }
-                }
-            }
-        }
+        Flyway flyway = Flyway.configure().dataSource(dataSource).load();
+        flyway.migrate();
+
+        return dataSource;
     }
 
     public void save(Person person) throws SQLException {
         try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement("insert into people (first_name, last_name) values (?, ?)", PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "insert into people (first_name, last_name) values (?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            )) {
                 statement.setString(1, person.getFirstName());
                 statement.setString(2, person.getLastName());
+
                 statement.executeUpdate();
 
-                ResultSet rs = statement.getGeneratedKeys();
-                rs.next();
-                person.setId(rs.getLong("id"));
+                try (ResultSet rs = statement.getGeneratedKeys()) {
+                    rs.next();
+                    person.setId(rs.getLong("id"));
+                }
             }
         }
     }
 
     public Person retrieve(long id) throws SQLException {
         try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement("select * from people where id = ?")) {
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "select * from people where id = ?"
+            )) {
                 statement.setLong(1, id);
-                ResultSet rs = statement.executeQuery();
 
-                if (rs.next()) {
-                    Person person = new Person();
-                    person.setId(rs.getLong("id"));
-                    person.setFirstName(rs.getString("first_name"));
-                    person.setLastName(rs.getString("last_name"));
-                    return person;
+                try (ResultSet rs = statement.executeQuery()) {
+                    rs.next();
+
+                    return mapFromResultSet(rs);
                 }
             }
         }
-        return null;
+    }
+
+    private Person mapFromResultSet(ResultSet rs) throws SQLException {
+        Person person = new Person();
+        person.setId(rs.getLong("id"));
+        person.setFirstName(rs.getString("first_name"));
+        person.setLastName(rs.getString("last_name"));
+        return person;
+    }
+
+    public List<Person> listByLastName(String lastName) throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "select * from people where last_name = ?"
+            )) {
+                statement.setString(1, lastName);
+                try (ResultSet rs = statement.executeQuery()) {
+                    ArrayList<Person> people = new ArrayList<>();
+
+                    while (rs.next()) {
+                        people.add(mapFromResultSet(rs));
+                    }
+
+                    return people;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        PersonDao dao = new PersonDao(createDataSource());
+
+        System.out.println("Please enter a last name: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String lastName = scanner.nextLine().trim();
+
+        System.out.println(dao.listByLastName(lastName));
+
     }
 }
